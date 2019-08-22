318b81f377703827126ec0ae35caf056dd1d1d81
Dan Phillips <dlphillips@gmail.com>
2019-08-22 12:56:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+
+  const state = {
+    lat: 33.885498,
+    lng: -78.446958,
+    zoom: 13,
+  }
+
+  const position = [state.lat, state.lng];
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simple react-leaflet example started with create-react-app.
         </p>
         <a
           className="App-link"
@@ -18,6 +28,25 @@ function App() {
         >
           Learn React
         </a>
+        <a
+          className="App-link"
+          href="https://react-leaflet.js.org/en/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React-Leaflet
+        </a>
+        <Map center={position} zoom={state.zoom} id="mapid">
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              #4 Private Drive <br /> Ocean Isle Beach, NC.
+            </Popup>
+          </Marker>
+        </Map>
       </header>
     </div>
   );
